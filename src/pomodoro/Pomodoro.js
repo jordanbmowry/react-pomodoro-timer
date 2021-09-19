586b531404d26c318b31fed5c79aad4ac84f2756
jordanbmowry <jordan.mowry@gmail.com>
2021-09-18 18:41:48 -0700

    created SessionInfo.js component

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import SessionInfo from './SessionInfo';
 import classNames from '../utils/class-names';
 import useInterval from '../utils/useInterval';
 import { secondsToDuration } from '../utils/duration';
@@ -123,7 +124,7 @@ function Pomodoro({
     }
   }
 
-  function displayRemainMinutesText(session) {
+  function displayRemainingMinutesText(session) {
     if (!session) {
       return;
     } else {
@@ -235,32 +236,12 @@ function Pomodoro({
       </div>
       <div>
         {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
-        <div className='row mb-2'>
-          <div className='col'>
-            {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
-            <h2 data-testid='session-title'>
-              {displayFocusOrBreakText(session)}
-            </h2>
-            {/* TODO: Update message below correctly format the time remaining in the current session */}
-            <p className='lead' data-testid='session-sub-title'>
-              {displayRemainMinutesText(session)}
-            </p>
-          </div>
-        </div>
-        <div className='row mb-2'>
-          <div className='col'>
-            <div className='progress' style={{ height: '20px' }}>
-              <div
-                className='progress-bar'
-                role='progressbar'
-                aria-valuemin='0'
-                aria-valuemax='100'
-                aria-valuenow='0' // TODO: Increase aria-valuenow as elapsed time increases
-                style={{ width: '0%' }} // TODO: Increase width % as elapsed time increases
-              />
-            </div>
-          </div>
-        </div>
+        {/* To Do */}
+        <SessionInfo
+          session={session}
+          displayFocusOrBreakText={displayFocusOrBreakText}
+          displayRemainingMinutesText={displayRemainingMinutesText}
+        />
       </div>
     </div>
   );
---
+++ src/pomodoro/SessionInfo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function SessionInfo({
+  session,
+  displayFocusOrBreakText,
+  displayRemainingMinutesText,
+}) {
+  let sessionInfo;
+  if (!session) {
+    sessionInfo = null;
+  } else {
+    sessionInfo = (
+      <React.Fragment>
+        <div className='row mb-2'>
+          <div className='col'>
+            {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
+            <h2 data-testid='session-title'>
+              {displayFocusOrBreakText(session)}
+            </h2>
+            {/* TODO: Update message below correctly format the time remaining in the current session */}
+            <p className='lead' data-testid='session-sub-title'>
+              {displayRemainingMinutesText(session)}
+            </p>
+          </div>
+        </div>
+        <div className='row mb-2'>
+          <div className='col'>
+            <div className='progress' style={{ height: '20px' }}>
+              <div
+                className='progress-bar'
+                role='progressbar'
+                aria-valuemin='0'
+                aria-valuemax='100'
+                aria-valuenow='0' // TODO: Increase aria-valuenow as elapsed time increases
+                style={{ width: '0%' }} // TODO: Increase width % as elapsed time increases
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+  return sessionInfo;
+}
+
+export default SessionInfo;
